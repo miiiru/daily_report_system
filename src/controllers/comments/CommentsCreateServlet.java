87e45feb65774cb437bdc4bcd16466a7e7b19f6a
hanae.izumi <izumi.hanae.1@gmail.com>
2021-07-08 21:43:23 +0900

    edit CommentCreateServlet.java

--- src/controllers/comments/CommentsCreateServlet.java
+++ src/controllers/comments/CommentsCreateServlet.java
@@ -1,12 +1,20 @@
 package controllers.comments;
 
 import java.io.IOException;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Comment;
+import models.Employee;
+import models.validators.CommentValidator;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class CommentsCreateServlet
@@ -26,6 +34,34 @@ public class CommentsCreateServlet extends HttpServlet {
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    }
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
 
+            Comment c = new Comment();
+
+            c.setEmployee((Employee)request.getSession().getAttribute("login_employee"));
+            c.setContent(request.getParameter("content"));
+
+            List<String> errors = CommentValidator.validate(c);
+            if(errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("report", c);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/comments/new.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.persist(c);
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+
+                response.sendRedirect(request.getContextPath() + "/comments/index");
+            }
+    }
+    }
 }
